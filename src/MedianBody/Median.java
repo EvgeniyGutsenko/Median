8c66751c21af058976db516b671a3517a52bc283
EvgeniyGutsenko <97789152+EvgeniyGutsenko@users.noreply.github.com>
2022-02-21 17:11:41 +0300

    Working version

---
+++ src/MedianBody/Median.java
@@ -0,0 +1,51 @@
+package MedianBody;
+
+public class Median {
+
+    private Median() {
+    }
+
+    private static void sortArray(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[index]) {
+                    index = j;
+                }
+            }
+            int temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    private static void sortArray(double[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[index]) {
+                    index = j;
+                }
+            }
+            double temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static float median(int[] array) {
+        sortArray(array);
+        if (array.length % 2 != 0) return array[array.length / 2];
+        else {
+            return (float) (array[(array.length / 2) - 1] + array[array.length / 2]) / 2;
+        }
+    }
+
+    public static double median(double[] array) {
+        sortArray(array);
+        if (array.length % 2 != 0) return array[array.length / 2];
+        else {
+            return (array[(array.length / 2) - 1] + array[array.length / 2]) / 2;
+        }
+    }
+}
